ea90a6d067e0a544336d50d939e6d944b1fe6680
Ulisses <m.stunik@gmail.com>
2021-10-12 20:01:36 -0300

    update packages

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,13 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import * as express from "express";
-import { routes } from "./routes";
-import { User } from "user/User";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import * as express from 'express';
+import { routes } from './routes';
 
-createConnection().then(async connection => {
+createConnection().then(async (connection) => {
+  // create express app
+  const app = express();
+  app.use(express.json());
+  app.use(routes);
 
-    // create express app
-    const app = express();
-    app.use(express.json())
-    app.use(routes)
-
-    app.listen(3000, () =>
-        console.info("Server running on http://localhost:3000")
-    )
-
-
-}).catch(error => console.log(error));
+  app.listen(3000, () => console.info('Server running on http://localhost:3000'));
+}).catch((error) => console.log(error));
