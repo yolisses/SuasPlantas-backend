af852a06b30102498d44eca87546dd5147d489c3
Ulisses <m.stunik@gmail.com>
2021-10-12 17:30:22 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import * as express from "express";
+import { routes } from "./routes";
+
+createConnection().then(async connection => {
+
+    // create express app
+    const app = express();
+    app.use(express.json())
+    app.use(routes)
+
+    app.listen(3000, () =>
+        console.info("Server running on http://localhost:3000")
+    )
+
+
+}).catch(error => console.log(error));
