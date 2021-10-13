c2a26c73d16cd5d454dd45d2b5063ae561566c28
Ulisses <m.stunik@gmail.com>
2021-10-13 16:10:56 -0300

    leave without error thwroing for now

--- src/plant/getPlant.ts
+++ src/plant/getPlant.ts
@@ -1,7 +1,7 @@
+import * as createError from 'http-errors';
 import { Plant } from './PlantModel';
 
 export async function getPlant(id: number) {
   const plant = await Plant.findOne(id);
-  if (!plant) throw new Error('Plant not found');
   return plant;
 }
