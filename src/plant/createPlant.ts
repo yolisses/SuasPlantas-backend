0386e21ae6d6f469496c97145a189b66c417a875
Ulisses <m.stunik@gmail.com>
2021-10-13 20:59:25 -0300

    add image related things

--- src/plant/createPlant.ts
+++ src/plant/createPlant.ts
@@ -1,9 +1,22 @@
 import { Tag } from 'tag/Tag';
-import { IPlant } from './PlantInterface';
+import { error } from 'utils/error';
+import { IPlantInfo } from './PlantInterface';
 import { Plant } from './Plant';
 
-export async function createPlant(plant:IPlant) {
+interface ICreatePlantDTO extends IPlantInfo{
+  imagesCount:number
+}
+
+export async function createPlant(plant:ICreatePlantDTO) {
+  const { imagesCount } = plant;
+  if (!imagesCount) error(400, 'Images count not provided');
+  if (imagesCount < 1) error(400, 'Images count smaller than one');
+  if (imagesCount > 10) error(400, 'Images count bigger than 10');
   const result = await Plant.create(plant);
+  const images = [];
+  for (let i = 0; i < imagesCount; i++) {
+    images[i] = generateImageName();
+  }
   if (plant.tags) {
     const tags: Tag[] = await Tag.findByIds(plant.tags);
     result.tags = tags;
