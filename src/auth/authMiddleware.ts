4d9f61628c9e91d6faade64a5a6a064f2ac482d9
Ulisses <m.stunik@gmail.com>
2021-10-14 06:05:04 -0300

    copy auth related things

---
+++ src/auth/authMiddleware.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+// eslint-disable-next-line consistent-return
+export function authMiddleware(req, res, next) {
+  const secret = process.env.AUTH_SECRET;
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ error: 'No token provided' });
+  }
+
+  const parts = authHeader.split(' ');
+  if (parts.length !== 2) {
+    return res.status(401).send({ error: 'Token with invalid number of segments' });
+  }
+
+  const [scheme, token] = parts;
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ error: 'Malformatted token' });
+  }
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: err });
+    }
+
+    req.userId = decoded.id;
+    return next();
+  });
+}
