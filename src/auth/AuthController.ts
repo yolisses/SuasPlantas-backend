1ed8f197d98485ba4f9196bbdea044f494d83c1a
Ulisses <m.stunik@gmail.com>
2021-10-14 07:25:03 -0300

    fix auth send

--- src/auth/AuthController.ts
+++ src/auth/AuthController.ts
@@ -3,6 +3,7 @@ import { signIn } from "./signIn";
 export const AuthController = {
     async signIn(req, res) {
         const { googleToken } = req.body
-        signIn(googleToken)
+        const user = await signIn(googleToken)
+        return res.send(user)
     }
 }
