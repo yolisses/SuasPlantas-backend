c8c77a394e1556c8cbe9d90b2404296b22ccdcc8
Ulisses <m.stunik@gmail.com>
2021-10-15 06:04:11 -0300

    add utils

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,12 @@ import { Router } from 'express';
 import { plantsRoutes } from 'plant/plantRoutes';
 import { userRoutes } from 'user/userRoutes';
 import { authRoutes } from 'auth/authRoutes';
+import { ping } from 'misc/ping';
 
 export const routes = Router();
 
 routes.use('/plants', plantsRoutes);
 routes.use('/users', userRoutes);
 routes.use('/auth', authRoutes);
+
+routes.get('/ping', ping);
