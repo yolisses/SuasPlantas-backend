8edf9a366236ea584333b60b09f45e0f7ad3b2ba
Ulisses <m.stunik@gmail.com>
2021-10-15 16:51:18 -0300

    rename

--- src/plant/getPlant.ts
+++
@@ -1,8 +0,0 @@
-import { error } from 'utils/error';
-import { Plant } from './Plant';
-
-export async function getPlant(id: number) {
-  const plant = await Plant.findOne(id);
-  if (!plant) error(404, 'Plant not found');
-  return plant;
-}
