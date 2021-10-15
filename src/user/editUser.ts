569fe690ec13c9e86a9340bfca4647b294ec821d
Ulisses <m.stunik@gmail.com>
2021-10-15 15:57:09 -0300

    fix crash

--- src/user/editUser.ts
+++ src/user/editUser.ts
@@ -3,17 +3,29 @@ import { User, UserId } from "./User";
 
 interface IUserCreationDTO {
   name: string;
+  instagramUsername?: string;
+  whatsappNumber?: number;
   description: string;
 }
 
 export async function editUser(
   userId: UserId,
-  { name, description }: IUserCreationDTO
+  { name, description, instagramUsername, whatsappNumber }: IUserCreationDTO
 ): Promise<User> {
   const user = await User.findOne(userId);
   if (!user) error(404, "User not found");
-  user.name = name;
-  user.description = description;
-  user.save();
+  if (name) {
+    user.name = name;
+  }
+  if (description !== undefined) {
+    user.description = description;
+  }
+  if (instagramUsername !== undefined) {
+    user.instagramUsername = instagramUsername;
+  }
+  if (whatsappNumber !== undefined) {
+    user.whatsappNumber = whatsappNumber;
+  }
+  await user.save();
   return user;
 }
