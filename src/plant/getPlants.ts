c5b78ffceb36f5a8b27290baa487b4bc3556e1c4
Ulisses <m.stunik@gmail.com>
2021-10-15 08:22:22 -0300

    send user id with plant

--- src/plant/getPlants.ts
+++ src/plant/getPlants.ts
@@ -1,5 +1,30 @@
+import { error } from 'utils/error';
 import { Plant } from './Plant';
 
-export async function getPlants({ page }) {
-    return Plant.find({ relations: ['images'], loadEagerRelations: true });
+
+const take = 2
+export async function getPlants({ page: pageParam }) {
+    const page = Number(pageParam)
+    if (!pageParam) error(400, 'Plants page not provided')
+    const skip = page * take
+
+    const data = await Plant.findAndCount({
+        take,
+        skip,
+        order: { createdAt: -1 },
+        relations: ['images'],
+        loadEagerRelations: true,
+        loadRelationIds: true
+    });
+
+    const totalPages = Math.ceil(data[1] / take)
+    const nextPage = page < totalPages ? page + 1 : null
+    return {
+        pageParam,
+        page,
+        totalPages,
+        totalCount: data[1],
+        nextPage,
+        content: data[0]
+    }
 }
