8b05fdf375e3c8a3eb5dc566cb59663b2b14d693
Ulisses <m.stunik@gmail.com>
2021-10-15 10:38:24 -0300

    cleanning

--- src/demiurge/user/getUser.ts
+++
@@ -1,7 +0,0 @@
-import { RequestHandler } from 'express';
-import { User } from './User';
-
-export const getUser: RequestHandler = async (req, res) => {
-  const users = await User.findOne(req.params.id);
-  return res.send(users);
-};
