491c1dd74be67ca124847b6c398037075737f992
Ulisses <m.stunik@gmail.com>
2021-10-16 18:08:15 -0300

    add images upload

--- src/plant/getPlantImageUploadLink.ts
+++ src/plant/getPlantImageUploadLink.ts
@@ -1,9 +1,7 @@
 import { UserId } from "user/User";
-import * as uuid from "uuid-random";
 import { s3 } from "vendor/s3";
 
-export async function createPlantImageUpdateLink(userId: UserId) {
-  const key = `uploads/${uuid()}.webp`;
+export async function getPlantImageUpdateLink(key: string, userId: UserId) {
   const bucketName = process.env.AWS_BUCKET_NAME;
   const oneHour = 60 * 60;
 
