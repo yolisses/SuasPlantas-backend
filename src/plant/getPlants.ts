d1adcbbcd8d0d9f02502c8ac1594114fd44109b1
Ulisses <m.stunik@gmail.com>
2021-10-16 06:46:03 -0300

    paginate

--- src/plant/getPlants.ts
+++ src/plant/getPlants.ts
@@ -1,30 +1,34 @@
-import { error } from 'utils/error';
-import { Plant } from './Plant';
+import { FindConditions } from "typeorm";
+import { Plant } from "./Plant";
 
+interface GetPlantsParams {
+  page: number;
+  sell?: boolean;
+  swap?: boolean;
+}
+const take = 2;
+export async function getPlants({ page, sell, swap }: GetPlantsParams) {
+  const skip = page * take;
 
-const take = 2
-export async function getPlants({ page: pageParam }) {
-    const page = Number(pageParam)
-    if (!pageParam) error(400, 'Plants page not provided')
-    const skip = page * take
+  const where: FindConditions<Plant> = {};
 
-    const data = await Plant.findAndCount({
-        take,
-        skip,
-        order: { createdAt: -1 },
-        relations: ['images'],
-        loadEagerRelations: true,
-        loadRelationIds: true
-    });
+  const data = await Plant.findAndCount({
+    take,
+    skip,
+    where,
+    order: { createdAt: -1 },
+    relations: ["images"],
+    loadEagerRelations: true,
+    loadRelationIds: true,
+  });
 
-    const totalPages = Math.ceil(data[1] / take)
-    const nextPage = page < totalPages ? page + 1 : null
-    return {
-        pageParam,
-        page,
-        totalPages,
-        totalCount: data[1],
-        nextPage,
-        content: data[0]
-    }
+  const totalPages = Math.ceil(data[1] / take);
+  const nextPage = page < totalPages ? page + 1 : null;
+  return {
+    page,
+    totalPages,
+    totalCount: data[1],
+    nextPage,
+    content: data[0],
+  };
 }
