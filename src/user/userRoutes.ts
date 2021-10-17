d8e9d695192e3a308326b3d1058a3a4046b37c8a
Ulisses <m.stunik@gmail.com>
2021-10-17 14:18:24 -0300

    fix remove user

--- src/user/userRoutes.ts
+++ src/user/userRoutes.ts
@@ -7,5 +7,5 @@ export const userRoutes = Router();
 userRoutes.get("/:id", UserController.getOne);
 userRoutes.patch("/", authMiddleware, UserController.edit);
 userRoutes.patch("/edit-location", authMiddleware, UserController.editLocation);
-userRoutes.delete("/:id", authMiddleware, UserController.remove);
+userRoutes.delete("/", authMiddleware, UserController.remove);
 userRoutes.get("/:userId/plants", UserController.getPlants);
