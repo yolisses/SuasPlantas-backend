74d43708ff089b80dfdb72ee7d3614b814ac6ec8
Ulisses <m.stunik@gmail.com>
2021-10-18 14:49:26 -0300

    validate user

--- src/plant/editPlant.ts
+++ src/plant/editPlant.ts
@@ -8,8 +8,8 @@ interface IPlantCreationDTO {
   name: string;
   swap: boolean;
   tags: string[];
-  price?: number;
-  amount?: number;
+  price: number;
+  amount: number;
   donate: boolean;
   description: string;
 }
@@ -17,34 +17,11 @@ interface IPlantCreationDTO {
 export async function editPlant(
   userId: number,
   plantId: PlantId,
-  plant: IPlantCreationDTO
+  { name, description, amount, price, swap, donate }: IPlantCreationDTO
 ) {
-  const { name, description, amount, price, swap, donate } = plant;
+  const plant = await Plant.findOneOrFail(plantId);
 
-  const result = await Plant.findOneOrFail(plantId);
+  if (plant.userId !== userId) error(403, "Plant edit by unauthorized user");
 
-  if (result.userId !== userId) error(403, "Plant edit by unauthorized user");
-
-  // const result = Plant.find({
-  //   name,
-  //   description,
-  //   amount,
-  //   price,
-  //   swap,
-  //   donate,
-  // });
-
-  const user = await User.findOneOrFail(userId);
-  result.user = user;
-  result.city = user.city;
-  result.state = user.state;
-  result.location = user.location;
-
-  if (plant.tags) {
-    const validatedTags = plant.tags.filter((tag) => validTags.has(tag));
-    const tags: Tag[] = await Tag.findByIds(validatedTags);
-    result.tags = tags;
-  }
-
-  return await result.save();
+  return await plant.save();
 }
