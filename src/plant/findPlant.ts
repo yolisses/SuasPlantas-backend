e546892fc6ea24ad9f948e7d974abac8ed4b5be7
Ulisses <m.stunik@gmail.com>
2021-10-18 07:55:51 -0300

    abstract plant id

--- src/plant/findPlant.ts
+++ src/plant/findPlant.ts
@@ -1,7 +1,7 @@
 import { error } from "utils/error";
-import { Plant } from "./Plant";
+import { Plant, PlantId } from "./Plant";
 
-export async function findPlant(id: number) {
+export async function findPlant(id: PlantId) {
   const plant = await Plant.findOne(id);
   if (!plant) error(404, "Plant not found");
   return plant;
