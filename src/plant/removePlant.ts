16cd919513bdd30bf152d51c8d7b2d2b8498fcb9
Ulisses <m.stunik@gmail.com>
2021-10-18 09:59:44 -0300

    fix plant remotion

--- src/plant/removePlant.ts
+++ src/plant/removePlant.ts
@@ -6,5 +6,5 @@ export async function removePlant(id: PlantId, userId: UserId) {
   const plant = await Plant.findOne(id);
   if (!plant) error(404, "Plant not found");
   if (plant.userId !== userId) error(403, "Plant remove by unauthorized user");
-  plant.softRemove();
+  await plant.softRemove();
 }
