9c3d9bffe3889e1cbd64c5e868cccd33ade9ba68
Ulisses <m.stunik@gmail.com>
2021-10-18 09:28:41 -0300

    fix send link route

--- src/plant/plantRoutes.ts
+++ src/plant/plantRoutes.ts
@@ -6,12 +6,14 @@ export const plantsRoutes = Router();
 
 plantsRoutes.get("/", PlantController.get);
 plantsRoutes.get("/all", PlantController.getAll);
-plantsRoutes.get("/:id", PlantController.getOne);
-plantsRoutes.delete("/:id", PlantController.remove);
-plantsRoutes.patch("/:plantId", authMiddleware, PlantController.edit);
-plantsRoutes.post("/", authMiddleware, PlantController.create);
 plantsRoutes.get(
   "/image-link",
   authMiddleware,
   PlantController.getImageUploadLink
 );
+// Should be after the more specifics
+plantsRoutes.get("/:id", PlantController.getOne);
+
+plantsRoutes.delete("/:id", PlantController.remove);
+plantsRoutes.patch("/:plantId", authMiddleware, PlantController.edit);
+plantsRoutes.post("/", authMiddleware, PlantController.create);
