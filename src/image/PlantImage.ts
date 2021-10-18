ccb896d9a7b60ec07a60631c586ac0d4bf50d238
Ulisses <m.stunik@gmail.com>
2021-10-18 07:26:18 -0300

    cascade plant remove

--- src/image/PlantImage.ts
+++ src/image/PlantImage.ts
@@ -1,13 +1,11 @@
-import { Plant } from 'plant/Plant';
-import {
-  BaseEntity, Entity, ManyToOne, PrimaryColumn,
-} from 'typeorm';
+import { Plant } from "plant/Plant";
+import { BaseEntity, Entity, ManyToOne, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class PlantImage extends BaseEntity {
-    @PrimaryColumn()
-      uri:string;
+  @PrimaryColumn()
+  uri: string;
 
-    @ManyToOne(() => Plant, (plant) => plant.images)
-      plant: Plant;
+  @ManyToOne(() => Plant, (plant) => plant.images, { onDelete: "CASCADE" })
+  plant: Plant;
 }
