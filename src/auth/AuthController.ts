94ef444b484e831074cc44bb26e6b382275df9c4
Ulisses <m.stunik@gmail.com>
2021-11-09 08:01:17 -0300

    fix cors and pass the auth jwt as cookie

--- src/auth/AuthController.ts
+++ src/auth/AuthController.ts
@@ -1,12 +1,18 @@
+import { Response } from 'express';
 import { generateToken } from './generateToken';
 import { signIn } from './signIn';
 
 export const AuthController = {
-  async signIn(req, res) {
+  async signIn(req, res: Response) {
     const { ip } = req;
     const { googleToken } = req.body;
     const user = await signIn(googleToken, ip);
     const token = generateToken({ id: user.id });
+    res.cookie('auth_login_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+    });
+
     return res.send({ user, token });
   },
 };
--- src/corsOptions.ts
+++ src/corsOptions.ts
@@ -1,4 +1,8 @@
-export const corsOptions = {
+import { CorsOptions } from 'cors';
+
+export const corsOptions: CorsOptions = {
   origin: 'http://localhost:3000',
+  allowedHeaders: ['X-Request', 'content-type'],
+  credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
