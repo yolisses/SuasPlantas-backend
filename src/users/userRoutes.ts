26290fa6250f777d22ecee9f2dfef635e5263b87
Ulisses <m.stunik@gmail.com>
2021-12-11 14:32:22 -0300

    lint

--- src/users/userRoutes.ts
+++ src/users/userRoutes.ts
@@ -3,9 +3,9 @@ import { UserController } from './UserController';
 
 export const userRoutes = Router();
 
-userRoutes.post('/', UserController.signIn);
-userRoutes.get('/:id', UserController.getOne);
 userRoutes.patch('/', UserController.edit);
-userRoutes.patch('/edit-location', UserController.editLocation);
+userRoutes.post('/', UserController.signIn);
 userRoutes.delete('/', UserController.remove);
+userRoutes.get('/:id', UserController.getOne);
 userRoutes.get('/:userId/plants', UserController.getPlants);
+userRoutes.patch('/edit-location', UserController.editLocation);
