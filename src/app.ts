69f0ede5ae8ae61d8b71f9b6534b500366575761
Ulisses <m.stunik@gmail.com>
2021-12-11 08:04:40 -0300

    make a to do list

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 /* eslint-disable import/first */
-import 'reflect-metadata';
-import 'regenerator-runtime';
-import dotenv from 'dotenv';
+import "reflect-metadata";
+import "regenerator-runtime";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-import 'express-async-errors';
-import { createConnection } from 'typeorm';
-import express from 'express';
-import { dbConfig } from 'db/dbConfig';
-import { corsOptions } from 'corsOptions';
-import cors from 'cors';
-import { errorMiddleware } from './errorMiddleware';
-import { routes } from './routes';
+import "express-async-errors";
+import { createConnection } from "typeorm";
+import express from "express";
+import { dbConfig } from "db/dbConfig";
+import { corsOptions } from "corsOptions";
+import cors from "cors";
+import { errorMiddleware } from "./errorMiddleware";
+import { routes } from "./routes";
 
 createConnection(dbConfig)
   .then(async () => {
@@ -25,6 +25,8 @@ createConnection(dbConfig)
     app.use(errorMiddleware);
 
     const port = parseInt(process.env.PORT, 10);
-    app.listen(port, () => console.info(`Server running on http://localhost:${port}`));
+    app.listen(port, () =>
+      console.info(`Server running on http://localhost:${port}`)
+    );
   })
   .catch((error) => console.log(error));
--- src/corsOptions.ts
+++ src/corsOptions.ts
@@ -1,8 +1,8 @@
-import { CorsOptions } from 'cors';
+import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
-  origin: 'http://localhost:3000',
-  allowedHeaders: ['X-Request', 'content-type'],
+  origin: "http://localhost:3000",
+  allowedHeaders: ["X-Request", "content-type"],
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
