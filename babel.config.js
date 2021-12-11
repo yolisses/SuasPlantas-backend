6329cb57399d1d61ce706a770fc98c133c3a5676
Ulisses <m.stunik@gmail.com>
2021-12-11 09:05:55 -0300

    remove path pluggins

--- babel.config.js
+++ babel.config.js
@@ -4,21 +4,5 @@ module.exports = {
     'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['babel-plugin-module-resolver', {
-      alias: {
-        db: './src/db',
-        tag: './src/tag',
-        auth: './src/auth',
-        data: './src/data',
-        like: './src/like',
-        user: './src/user',
-        image: './src/image',
-        plant: './src/plant',
-        utils: './src/utils',
-        upload: './src/upload',
-        vendor: './src/vendor',
-        location: './src/location',
-      },
-    }],
   ],
 };
