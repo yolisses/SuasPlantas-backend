27ce0c081d520f48c86dc4d9c3efd79b0d57d5a4
Ulisses <m.stunik@gmail.com>
2021-12-11 09:49:20 -0300

    refatorate

--- src/vendor/awsConfig.ts
+++ src/vendor/awsConfig.ts
@@ -1,14 +1,10 @@
 import { AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY } from '../env/env';
 
 function getAwsConfig() {
-  const region = AWS_REGION;
-  const accessKeyId = AWS_ACCESS_KEY_ID;
-  const secretAccessKey = AWS_SECRET_ACCESS_KEY;
-
   return {
-    region,
-    accessKeyId,
-    secretAccessKey,
+    region: AWS_REGION,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
     signatureVersion: 'v4',
   };
 }
