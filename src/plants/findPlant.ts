c8c22babb4362bfb93f2fb7d4d0c35823b9b2adb
Ulisses <m.stunik@gmail.com>
2021-12-14 14:14:12 -0300

    add user to find plant

--- src/plants/findPlant.ts
+++ src/plants/findPlant.ts
@@ -2,7 +2,7 @@ import { validateFound } from '../utils/validateFound';
 import { Plant, PlantId } from './Plant';
 
 export async function findPlant(id: PlantId) {
-  const plant = await Plant.findOne(id);
+  const plant = await Plant.findOne(id, { relations: ['user'] });
   validateFound({ plant });
   return plant;
 }
