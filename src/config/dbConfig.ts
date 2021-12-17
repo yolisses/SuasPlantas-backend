beb36210b608fe67e247fd5c08aa44b3da260b88
Ulisses <m.stunik@gmail.com>
2021-12-17 08:30:49 -0300

    fix build

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,12 +1,5 @@
 import { ConnectionOptions } from 'typeorm';
-import {
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_SSL,
-  DB_USER,
-} from './env';
+import { DATABASE_URL } from './env';
 import { Tag } from '../plants/Tag';
 import { User } from '../users/User';
 import { Image } from '../upload/Image';
@@ -15,13 +8,11 @@ import { Like } from '../plants/like/Like';
 import { Session } from '../signIn/Session';
 
 export const dbConfig: ConnectionOptions = {
-  database: DB_NAME,
-  username: DB_USER,
-  password: DB_PASSWORD,
   type: 'postgres',
-  port: parseInt(DB_PORT, 10),
-  host: DB_HOST,
-  ssl: DB_SSL.toLowerCase() === 'true',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  url: DATABASE_URL,
 
   synchronize: true,
   logging: false,
