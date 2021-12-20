d15a6c5d4615bf5a5887e08e5358043f6352aeca
Ulisses <m.stunik@gmail.com>
2021-12-20 16:31:08 -0300

    try use origin from req

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ createConnection(dbConfig)
     app.use(sessionConfig(connection));
     app.use((req, res, next) => {
       res.header('Access-Control-Allow-Credentials', 'true');
-      res.header('Access-Control-Allow-Origin', 'https://suasplantas.com');
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
       res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
       next();
