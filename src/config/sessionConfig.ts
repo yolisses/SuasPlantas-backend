62325ac9a52ee95a111694fe999efc9ef13198d6
Ulisses <m.stunik@gmail.com>
2021-12-23 16:32:12 -0300

    add www. to session domain

--- src/config/sessionConfig.ts
+++ src/config/sessionConfig.ts
@@ -12,7 +12,7 @@ export const sessionConfig = (connection) => session({
   saveUninitialized: false,
   cookie: {
     maxAge: sessionMaxAge,
-    domain: 'suasplantas.com',
+    domain: 'www.suasplantas.com',
   },
   store: new TypeormStore().connect(connection.getRepository(Session)),
 });
