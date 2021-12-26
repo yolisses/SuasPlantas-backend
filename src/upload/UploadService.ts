39332b033ddae7bc37c9bab5ae7edff0ca09dba0
Ulisses <m.stunik@gmail.com>
2021-12-26 14:07:44 -0300

    fix plant image

--- src/upload/UploadService.ts
+++ src/upload/UploadService.ts
@@ -1,12 +1,11 @@
-import uuid from 'uuid-random';
 import { s3 } from '../vendor/s3';
 import { UserId } from '../users/User';
 import { AWS_BUCKET_NAME } from '../config/env';
 import { oneHourInMinutes } from '../utils/oneHourInMinutes';
 
-export async function generateUploadLink(userId: UserId) {
+export async function generateUploadLink(key :string, userId: UserId) {
   const params = {
-    Key: `uploads/${uuid()}`,
+    Key: `uploads/${key}`,
     Expires: oneHourInMinutes,
     Bucket: AWS_BUCKET_NAME,
     Metadata: { userId: `${userId}` },
