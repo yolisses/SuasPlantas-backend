27567a3f2f8c8ec5d5e6b9a65468112e7f4dfcf8
Ulisses <m.stunik@gmail.com>
2021-12-28 15:19:10 -0300

    add tags to find plant

--- src/plants/findPlant.ts
+++ src/plants/findPlant.ts
@@ -3,7 +3,7 @@ import { Plant, PlantId } from './Plant';
 
 export async function findPlant(id: PlantId) {
   const plant = await Plant.findOne(id, {
-    relations: ['user', 'images'],
+    relations: ['user', 'images', 'tags'],
   });
   validateFound({ plant });
   return plant;
