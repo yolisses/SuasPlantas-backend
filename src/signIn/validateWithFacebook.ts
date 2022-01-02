6ee612b9ea2613560dcf91a0b6ec24694d38a872
Ulisses <m.stunik@gmail.com>
2022-01-02 17:09:33 -0300

    fix build

--- src/signIn/validateWithFacebook.ts
+++ src/signIn/validateWithFacebook.ts
@@ -2,13 +2,20 @@ import axios from 'axios';
 import { error } from '../utils/error';
 import { ValidationResponse } from './ValidationResponse';
 
+interface facebookResponse
+{
+  id: string,
+  name: string,
+  email: string,
+  picture: { data: { url: string } }
+}
+
 // eslint-disable-next-line consistent-return
 export async function validateWithFacebook(accessToken: string):Promise<ValidationResponse> {
   try {
     const res = await axios.get(`https://graph.facebook.com/v12.0/me?fields=id%2Cname%2Cemail%2Cpicture.type(large)%7Burl%7D&access_token=${accessToken}`);
-    console.log(res.data);
-    const { email, name, picture } = res.data;
-    return { email, name, picture };
+    const { email, name, picture: { data: { url } } } = res.data as facebookResponse;
+    return { email, name, picture: url };
   } catch (err) {
     error(400, err.message);
   }
