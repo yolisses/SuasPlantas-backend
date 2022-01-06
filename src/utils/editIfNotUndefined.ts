e1ab255b0fb9d6117c959fd69cda7ff29bb1b69b
Ulisses <m.stunik@gmail.com>
2022-01-06 18:13:13 -0300

    fix profile edit

--- src/utils/editIfNotUndefined.ts
+++ src/utils/editIfNotUndefined.ts
@@ -1,7 +1,7 @@
 export function editIfNotUndefined(obj :any, values:{[key:string]:any}) {
   // eslint-disable-next-line no-restricted-syntax
   for (const key in values) {
-    if (values[key] === undefined) {
+    if (values[key] !== undefined) {
       // eslint-disable-next-line no-param-reassign
       obj[key] = values[key];
     }
