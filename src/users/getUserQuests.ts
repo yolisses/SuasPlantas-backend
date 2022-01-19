741c84b24c3d216628334e03214f54ad1eb32149
Ulisses <m.stunik@gmail.com>
2022-01-19 10:13:56 -0300

    fix build

--- src/users/getUserQuests.ts
+++ src/users/getUserQuests.ts
@@ -2,5 +2,5 @@ import { Quest } from '../quests/Quest';
 import { UserId } from './User';
 
 export async function getUserQuests(userId:UserId) {
-  return Quest.find({ userId, order: { createdAt: 'ASC' } });
+  return Quest.find({ where: { userId }, order: { createdAt: 'ASC' } });
 }
