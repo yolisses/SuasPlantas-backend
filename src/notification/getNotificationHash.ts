3d762105be416e70616aba4a6983a8e915afa1c9
Ulisses <m.stunik@gmail.com>
2022-01-26 23:01:35 -0300

    fix get notification hash

--- src/notification/getNotificationHash.ts
+++ src/notification/getNotificationHash.ts
@@ -2,12 +2,30 @@ import crypto from 'crypto';
 import { UserId, User } from '../users/User';
 import { ONESIGNAL_REST_API_KEY } from '../config/env';
 
-export async function getNotificationHash(userId:UserId) {
-  const user = await User.findOneOrFail(userId);
-  const hmac = crypto.createHmac('sha256', ONESIGNAL_REST_API_KEY);
-  console.log(user.email);
-  hmac.update('stunik@gmail.com');
-  const result = hmac.digest('hex');
-  console.log(result);
-  return result;
+export interface GetNotificationHashResult{
+  id:string
+  email:string
+  idHash:string
+  emailHash:string
+}
+
+export async function getNotificationHash(userId:UserId):Promise<GetNotificationHashResult> {
+  const { email } = await User.findOneOrFail(userId, { select: ['id', 'email'] });
+  const id = `${userId}`;
+  console.log(id);
+  const idHash = crypto
+    .createHmac('sha256', ONESIGNAL_REST_API_KEY)
+    .update(id)
+    .digest('hex');
+  const emailHash = crypto
+    .createHmac('sha256', ONESIGNAL_REST_API_KEY)
+    .update(email)
+    .digest('hex');
+
+  return {
+    id,
+    email,
+    idHash,
+    emailHash,
+  };
 }
