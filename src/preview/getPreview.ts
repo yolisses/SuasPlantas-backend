87e80d333820ee962a553ffce553627387efdee0
Ulisses <m.stunik@gmail.com>
2022-02-05 08:40:05 -0300

    return undefined if user has no deleted at

--- src/preview/getPreview.ts
+++ src/preview/getPreview.ts
@@ -1,6 +1,17 @@
+import { Preview } from './Preview';
 import { User } from '../users/User';
+import { validateFound } from '../utils/validateFound';
 
-export async function getPreview(previewCode:string) {
-  if (!previewCode) return;
-  return User.findOne({ where: { id: 3 }, relations: ['plants', 'quests'] });
+export async function getPreview(previewCode:string):Promise<User|undefined> {
+  const preview = await Preview.findOne(previewCode);
+  console.log(preview);
+  validateFound({ preview });
+  const id = preview.userId;
+  const user = await User.findOne({
+    where: { id },
+    relations: ['plants', 'quests'],
+    withDeleted: true,
+  });
+  if (user.deletedAt) return user;
+  return undefined;
 }
