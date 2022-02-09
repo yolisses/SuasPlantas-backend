9743566f6bc2c7ea0ba3eda60820f90534b90630
Ulisses <m.stunik@gmail.com>
2022-02-09 11:01:08 -0300

    add new preview code in create user by profile

--- src/users/createUserByProfile.ts
+++ src/users/createUserByProfile.ts
@@ -1,9 +1,11 @@
+import { getNewPreviewCode } from '../preview/getNewPreviewCode';
 import { User } from './User';
 
 export async function createUserByProfile({ name, image }:{name:string, image:string}) {
   const user = await User.create({
     name,
     image,
+    preview: getNewPreviewCode(),
   }).save();
   return user.softRemove();
 }
