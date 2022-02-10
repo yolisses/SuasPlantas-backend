54230624872f345741a3b983de35e5cebe842f8d
Ulisses <m.stunik@gmail.com>
2022-02-10 12:16:48 -0300

    specify default port

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,6 @@ createConnection(dbConfig)
     // last
     app.use(errorMiddleware);
 
-    const port = parseInt(PORT, 10);
-    app.listen(port, () => console.info(`Server running on http://localhost:${port}`));
+    app.listen(PORT || 3001, () => console.info(`Server running on http://localhost:${port}`));
   })
   .catch((error) => console.error(error));
