498c9fdd9342fe6d95d0ded7b364e35bb3295718
Ulisses <m.stunik@gmail.com>
2022-02-12 21:51:20 -0300

    add city and state to createUserByProfile

--- src/users/createUserByProfile.ts
+++ src/users/createUserByProfile.ts
@@ -1,10 +1,21 @@
 import { getNewPreviewCode } from '../preview/getNewPreviewCode';
 import { User } from './User';
 
-export async function createUserByProfile({ name, image }:{name:string, image:string}) {
+interface CreateUserByProfileDTO{
+  name:string
+  image:string
+  city?:string
+  state?:string
+}
+
+export async function createUserByProfile({
+  name, image, city, state,
+}:CreateUserByProfileDTO) {
   const user = await User.create({
     name,
     image,
+    city: city || undefined,
+    state: state || undefined,
     preview: getNewPreviewCode(),
   }).save();
   return user.softRemove();
