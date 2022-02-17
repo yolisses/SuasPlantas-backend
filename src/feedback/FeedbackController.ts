91050baf6b7da7e9d223e645530bc5ed3c98a037
Ulisses <m.stunik@gmail.com>
2022-02-16 23:18:54 -0300

    fix ip on feedback creation

--- src/feedback/FeedbackController.ts
+++ src/feedback/FeedbackController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
+import { getReqIp } from '../users/getReqIp';
 import { addFeedback } from './FeedbackService';
 
 export const FeedbackController = {
   async add(req:Request, res:Response) {
-    const { ip } = req;
+    const ip = getReqIp(req);
     const { userId } = req.session;
     const feedback = await addFeedback(req.body, userId, ip);
     return res.send(feedback);
