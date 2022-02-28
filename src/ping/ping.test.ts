1cd5c5f72d285180c6487c1c4f86eb0a6d2fa5ad
Ulisses <m.stunik@gmail.com>
2022-02-27 22:56:26 -0300

    add body test on ping

--- src/ping/ping.test.ts
+++ src/ping/ping.test.ts
@@ -2,8 +2,21 @@ import req from 'supertest';
 import { app } from '../server';
 
 it('should ping a response', (done) => {
-  req(app).get('/ping').expect(200).then((res) => {
-    console.log(res);
-    done();
-  });
+  req(app)
+    .get('/ping')
+    .expect(200, done);
+});
+
+it('should ping return the body as sent', (done) => {
+  const body = { hello: 'world' };
+  req(app)
+    .get('/ping')
+    .send(body)
+    .expect(200)
+    .end((err, res) => {
+      if (err)done(err);
+      expect(res.body)
+        .toHaveProperty('body', body);
+      done();
+    });
 });
