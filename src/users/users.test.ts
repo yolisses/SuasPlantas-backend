53517a912d8b4726093871a2666aaa15b4971c61
Ulisses <m.stunik@gmail.com>
2022-02-28 12:19:38 -0300

    refatorate

--- src/users/users.test.ts
+++ src/users/users.test.ts
@@ -1,4 +1,16 @@
 import req from 'supertest';
-import { app } from '../server';
+import { app, server } from '../server';
+import { getUser } from './getUser';
+import { User } from './User';
 
-it('should return error if user is not authenticated', () => req(app).get('/users/me').expect(403));
+it(
+  'should return error if user is not authenticated',
+  () => req(app).get('/users/me').expect(403),
+);
+
+it('should return a user if the userId is valid', async () => {
+  const userId = 1;
+  const app = server();
+  const user = await getUser(userId);
+  expect(user).toBeInstanceOf(User);
+});
