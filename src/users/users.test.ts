ac46136941e377ac096dded84328164c7dc01b87
Ulisses <m.stunik@gmail.com>
2022-02-28 15:48:21 -0300

    refatorate

--- src/users/users.test.ts
+++ src/users/users.test.ts
@@ -4,6 +4,10 @@ import { app } from '../server';
 import { getUser } from './getUser';
 import { startDatabase } from '../database/startDatabase';
 
+beforeAll(async () => {
+  await startDatabase();
+});
+
 it(
   'should return error if user is not authenticated',
   () => req(app).get('/users/me').expect(403),
@@ -11,7 +15,6 @@ it(
 
 it('should return a user when userId is valid', async () => {
   const userId = 1;
-  await startDatabase();
   await User.create({ id: 1, name: 'test user', image: 'https://testimage' }).save();
   const user = await getUser(userId);
   expect(user).toBeInstanceOf(User);
@@ -19,7 +22,6 @@ it('should return a user when userId is valid', async () => {
 
 it('should return undefined when userId is valid', async () => {
   const userId = 9999;
-  await startDatabase();
   const user = await getUser(userId);
   expect(user).toBe(undefined);
 });
