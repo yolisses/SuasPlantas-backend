45065e3ee1d5ef744ba08f914e816b74596eb31c
Ulisses <m.stunik@gmail.com>
2022-03-01 21:05:57 -0300

    make all tests pass

--- src/chat/chat.test.ts
+++ src/chat/chat.test.ts
@@ -1,32 +1,114 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
+import { Chat } from './Chat';
+import { Message } from './Message';
+import { User } from '../users/User';
 import { sendMessage } from './sendMessage';
 import { mockMessages } from './mockMessages';
 import { getUserChats } from './getUserChats';
 import { getChatMessages } from './getChatMessages';
 import { findOrCreateChat } from './findOrCreateChat';
 import { startDatabase } from '../database/startDatabase';
-import { Chat } from './Chat';
-import { User } from '../users/User';
 
-// const messages = mockMessages;
 beforeAll(async () => {
   await startDatabase();
-  await Promise.all([1, 2].map(async (id) => User.create({ id, name: `user ${id}`, image: 'image' }).save()));
+  for (const id of [1, 2, 3, 4]) {
+    await User.create({ id, name: `user ${id}`, image: `image ${id}` }).save();
+  }
   await Chat.create({ id: 1, user1: 1, user2: 2 }).save();
-  const users = await User.find();
-  console.log(users);
+  await Chat.create({ id: 2, user1: 1, user2: 3 }).save();
+  await Chat.create({ id: 3, user1: 2, user2: 3 }).save();
+  await Message.create({
+    id: 1, text: '1 -> 2', senderId: 1, chatId: 1,
+  }).save();
+  await Message.create({
+    id: 2, text: '2 -> 1', senderId: 2, chatId: 1,
+  }).save();
+  await Message.create({
+    id: 3, text: '1 -> 3', senderId: 1, chatId: 2,
+  }).save();
+  await Message.create({
+    id: 4, text: '3 -> 1', senderId: 3, chatId: 2,
+  }).save();
+  await Message.create({
+    id: 5, text: '2 -> 3', senderId: 2, chatId: 3,
+  }).save();
+  await Message.create({
+    id: 6, text: '3 -> 2', senderId: 3, chatId: 3,
+  }).save();
+});
+
+it('should return user chats', async () => {
+  const userId = 1;
+  const chats = await getUserChats(userId);
+  expect(chats[0].lastTime > chats[1].lastTime);
+  expect(chats).toEqual(
+    [
+      {
+        id: 2,
+        userId: 3,
+        name: 'user 3',
+        image: 'image 3',
+        lastText: '3 -> 1',
+        lastUserId: 3,
+        lastTime: expect.any(Date),
+      },
+      {
+        id: 1,
+        userId: 2,
+        name: 'user 2',
+        image: 'image 2',
+        lastText: '2 -> 1',
+        lastUserId: 2,
+        lastTime: expect.any(Date),
+      },
+    ],
+  );
 });
 
 it('should return chat messages', async () => {
-  const res = await getChatMessages({ userIds: [1, 2] });
-  expect(res.content).toHaveLength(messages.length);
+  const res = await getChatMessages({ chatId: 1 });
+  expect(res).toMatchObject({
+    pageData: {
+      page: 0, totalPages: 1, totalCount: 2, nextPage: null,
+    },
+    content: [
+      {
+        id: 2,
+        text: '2 -> 1',
+        senderId: 2,
+        sender: undefined,
+        chatId: 1,
+        chat: undefined,
+        createdAt: expect.any(Date),
+      },
+      {
+        id: 1,
+        text: '1 -> 2',
+        senderId: 1,
+        sender: undefined,
+        chatId: 1,
+        chat: undefined,
+        createdAt: expect.any(Date),
+      }],
+  });
+  expect(res.content[0].createdAt > res.content[1].createdAt);
 });
 
-it('should return a chat', async () => {
+it('should return a present chat', async () => {
   const users = [1, 2];
   const chat = await findOrCreateChat(users);
-  expect(chat).toBeInstanceOf(Chat);
+  expect(chat).toMatchObject({
+    id: 1, user1: 1, user2: 2, messages: undefined,
+  });
+});
+
+it('should return a new chat', async () => {
+  const users = [3, 4];
+  const chat = await findOrCreateChat(users);
+  expect(chat).toMatchObject({
+    id: 4, user1: 3, user2: 4, messages: undefined,
+  });
 });
 
 it('should send messages', async () => {
@@ -36,10 +118,4 @@ it('should send messages', async () => {
     const { text, senderId } = message;
     await sendMessage({ chatId, text, senderId });
   }
-});
-
-it.only('should return user chats', async () => {
-  const userId = 1;
-  const chats = await getUserChats(userId);
-  console.log(chats);
 });
--- src/chat/getChatMessages.ts
+++ src/chat/getChatMessages.ts
@@ -1,28 +1,20 @@
 import { paginateResults } from '../common/paginateResults';
-import { UserId } from '../users/User';
+import { ChatId } from './Chat';
 import { Message } from './Message';
 
 interface GetChatMessagesParams{
-    userIds: [UserId, UserId]
-    page?:number
-    take?:number
+  page?: number;
+  take?: number;
+  chatId:ChatId
 }
 
-export async function getChatMessages({
-  userIds,
-  page = 0,
-  take = 50,
-}:GetChatMessagesParams) {
-  const [id1, id2] = userIds;
-  const query = Message.createQueryBuilder('message')
-    .setParameters({ id1, id2 })
-    .where('message.receiverId = :id1 and message.ownerId = :id2')
-    .orWhere('message.receiverId = :id2 and message.ownerId = :id1');
+export function getChatMessages({ chatId, page = 0, take = 50 }:GetChatMessagesParams) {
+  const query = Message.createQueryBuilder('message');
+  query.where('message.chatId = :chatId', { chatId });
 
   const skip = page * take;
   query
     .skip(skip)
     .take(take);
-
   return paginateResults(query.getManyAndCount(), { page, take });
 }
