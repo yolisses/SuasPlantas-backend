00f219e80a843e1723965aa7b49fa3cee985c9df
Ulisses <m.stunik@gmail.com>
2022-03-02 20:36:29 -0300

    fix also saw

--- src/chat/teste2.sql
+++ src/chat/teste2.sql
@@ -1,37 +1,35 @@
 SELECT
     rank,
-    id,
-    name,
-    card,
+    plant.id,
+    plant.name,
+    plant.card,
     city,
     state
 FROM
-    plants plant
-    LEFT JOIN (
-        SELECT
-            plant_id,
-            count(plant_id) AS rank
-        FROM
-            (
-                SELECT
-                    DISTINCT VIEW.user_id,
-                    plant_id
-                FROM
-                    VIEW
-                    INNER JOIN (
-                        SELECT
-                            DISTINCT user_id
-                        FROM
-                            VIEW
-                        WHERE
-                            VIEW.plant_id = $ 1
-                    ) AS vplanta2 ON VIEW.user_id = vplanta2.user_id
-            ) AS coisa
-        GROUP BY
-            plant_id
-    ) AS ranked ON plant.id = ranked.plant_id
+(
+        (
+            SELECT
+                view2.plant_id,
+                count(view2.plant_id) AS rank
+            FROM
+                (
+                    SELECT
+                        DISTINCT user_id
+                    FROM
+                        VIEW view1
+                    WHERE
+                        plant_id = 1
+                ) AS view1
+                LEFT JOIN VIEW view2 ON view2.user_id = view1.user_id
+            GROUP BY
+                view2.plant_id
+        ) AS ranked
+        LEFT JOIN plant ON plant.id = plant_id
+    )
+    LEFT JOIN "user" ON "user".id = plant.user_id
+WHERE
+    plant.id != 1
 ORDER BY
-    rank IS NULL,
     rank DESC
 LIMIT
     14
--- src/recommendation/alsoSaw.ts
+++ src/recommendation/alsoSaw.ts
@@ -2,43 +2,43 @@ import { getManager } from 'typeorm';
 import { Plant, PlantId } from '../plant/Plant';
 
 export async function alsoSaw(plantId: PlantId):Promise<Plant[]> {
+  // this query selects the users that viewed a given plant
+  // then select the plants that they viewed ordered by number of views from these users
   return getManager().query(`
 SELECT
-    rank,
-    id,
-    name,
-    card,
-    city,
-    state
+  rank,
+  plant.id,
+  plant.name,
+  plant.card,
+  city,
+  state
 FROM
-    plants plant
-    LEFT JOIN (
-        SELECT
-            plant_id,
-            count(plant_id) AS rank
-        FROM
-            (
-                SELECT
-                    DISTINCT VIEW.user_id,
-                    plant_id
-                FROM
-                    VIEW
-                    INNER JOIN (
-                        SELECT
-                            DISTINCT user_id
-                        FROM
-                            VIEW
-                        WHERE
-                            VIEW.plant_id = $1
-                    ) AS vplanta2 ON VIEW.user_id = vplanta2.user_id
-            ) AS coisa
-        GROUP BY
-            plant_id
-    ) AS ranked ON plant.id = ranked.plant_id
+(
+      (
+          SELECT
+              view2.plant_id,
+              count(view2.plant_id) AS rank
+          FROM
+              (
+                  SELECT
+                      DISTINCT user_id
+                  FROM
+                      VIEW view1
+                  WHERE
+                      plant_id = $1
+              ) AS view1
+              LEFT JOIN VIEW view2 ON view2.user_id = view1.user_id
+          GROUP BY
+              view2.plant_id
+      ) AS ranked
+      LEFT JOIN plant ON plant.id = plant_id
+  )
+  LEFT JOIN "user" ON "user".id = plant.user_id
+WHERE
+  plant.id != $1
 ORDER BY
-    rank IS NULL,
-    rank DESC
+  rank DESC
 LIMIT
-    14
+  14
 `, [plantId]);
 }
