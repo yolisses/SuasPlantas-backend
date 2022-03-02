13e972d62a83184c1865df3ab0a2b6c0c59142a5
Ulisses <m.stunik@gmail.com>
2022-03-02 18:45:13 -0300

    lint

--- src/utils/editIfNotUndefined.ts
+++ src/utils/editIfNotUndefined.ts
@@ -1,8 +1,8 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-syntax */
 export function editIfNotUndefined(obj :any, values:{[key:string]:any}) {
-  // eslint-disable-next-line no-restricted-syntax
   for (const key in values) {
     if (values[key] !== undefined) {
-      // eslint-disable-next-line no-param-reassign
       obj[key] = values[key];
     }
   }
