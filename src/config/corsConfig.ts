4399df29ccc7b4b6a26f5d5ec010fabb94695867
Ulisses <m.stunik@gmail.com>
2022-03-05 07:06:06 -0300

    add local address to corsorigin

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -6,6 +6,7 @@ export const corsOrigins = [
   'http://localhost:4000',
   'https://localhost:4000',
   'https://suasplantas.com',
+  'https://192.168.10.23:4000',
   'https://suasplantas.com.br',
   'https://www.suasplantas.com',
   'https://www.suasplantas.com.br',
