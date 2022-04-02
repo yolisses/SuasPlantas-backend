81cbce74de115ecf6332596067b53637dbb1f446
Ulisses <m.stunik@gmail.com>
2022-04-01 21:42:49 -0300

    fix build

--- src/plant/createPlant.ts
+++ src/plant/createPlant.ts
@@ -4,17 +4,13 @@ import { createImage } from './createImage';
 
 interface IPlantCreationDTO {
   name: string;
-  swap: boolean;
-  price?: number;
-  donate: boolean;
-  amount?: number;
   images: string[];
   description: string;
 }
 
 export async function createPlant(plant: IPlantCreationDTO, userId: number) {
   const {
-    name, description, amount, price, swap, donate, images,
+    name, description, images,
   } = plant;
 
   let card;
@@ -28,11 +24,7 @@ export async function createPlant(plant: IPlantCreationDTO, userId: number) {
 
   const result = Plant.create({
     name,
-    swap,
     card,
-    price,
-    amount,
-    donate,
     userId,
     description,
     images: imagesInstances,
